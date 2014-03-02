67e1cc0021bb550a00c4549c12fab645479e38d7
Gaige Pierce-Raison <gaige@chatsubo.net>
2014-03-01 22:02:56 -0800

    separated out event handling from fsmonitor

--- fsmonitor.go
+++ fsmonitor.go
@@ -8,13 +8,13 @@ import (
 )
 
 
-func AddWatch(path string, recursive bool, manager chan *Command) {
+func StartWatch(path string, recursive bool) (*fsnotify.Watcher, int, error) {
 	//TODO: Check and handle a non-recursive watch request
 
 	watched := 0
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error with establishing watcher, fsmonitor.go line 17:", err)
 	}
 
 	err = filepath.Walk(path, func (path string, info os.FileInfo, err error) error {
@@ -22,26 +22,24 @@ func AddWatch(path string, recursive bool, manager chan *Command) {
 			go func (path string) (err error) {
 				err = watcher.Watch(path)
 				if err != nil {
-					fmt.Println(err)
+					fmt.Printf("fsmonitor.go line 25\terror: %v: %v\n", err, path)
 					return err
 				}
+				watched++
 				return nil
 				// increment how many dirs are watched
-				//watched++
 			}(path)
 		}
 		return err
 	})
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error with walking filepath, fsmonitor.go line 36:", err)
 	}
 
-	for {
-		select {
-		case ev := <-watcher.Event:
-			fmt.Println(ev)
-		case err := <- watcher.Error:
-			fmt.Println(err)
-		}
-	}
+	return watcher, watched, err
+}
+
+
+func HandleWatch(watcher fsnotify.Watcher, manager chan *Command) {
+
 }
