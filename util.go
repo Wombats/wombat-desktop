9c0328e2b1ec5b05083206eccf51db1fe59b7ae8
Gaige Pierce-Raison <gaige@chatsubo.net>
2014-03-06 15:18:52 -0800

    utility function for dealing with excludes.
    HasMember() for with a []string and a string to see if string is in []string.
    
    CollectExcludes() with a set of paths will return a []string with the excludes.
       Used by fsmonitor

---
+++ util.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+
+func CollectExcludes(excludePaths []string) ([]string){
+
+	excludes := make([]string, 1, 1)
+
+	for i := 0; i < len(excludePaths); i++ {
+		exclude := excludePaths[i]
+		err := filepath.Walk(exclude, func(path string, info os.FileInfo, err error) (error) {
+			if (strings.HasPrefix(path, exclude)) {
+				excludes = append(excludes, path)
+			}
+			return err
+		})
+
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	return excludes
+}
+
+
+func HasMember(arr []string, item string) (bool) {
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == item {
+			return true
+		}
+	}
+	return false
+}
